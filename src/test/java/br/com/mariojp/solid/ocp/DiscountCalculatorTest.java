773003b89ea7d7b9ca185ea009b95c3e35ad9d3f
davim <davim@Sabi>
2025-09-02 19:41:34 -0300

    Atividade 2

--- src/test/java/br/com/mariojp/solid/ocp/DiscountCalculatorTest.java
+++ src/test/java/br/com/mariojp/solid/ocp/DiscountCalculatorTest.java
@@ -1,13 +1,26 @@
 package br.com.mariojp.solid.ocp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.EnumMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class DiscountCalculatorTest {
+
     @Test
     void partner_gets_12_percent() {
-        var calc = new DiscountCalculator();
-        assertEquals(88.0, calc.apply(100.0, CustomerType.PARTNER), 0.0001,
-            "PARTNER deveria ter 12% de desconto");
+        Map<CustomerType, DiscountPolicy> policies = new EnumMap<>(CustomerType.class);
+        policies.put(CustomerType.REGULAR, new RegularPolicy());
+        policies.put(CustomerType.PREMIUM, new PremiumPolicy());
+        policies.put(CustomerType.PARTNER, new PartnerPolicy());
+
+        DiscountCalculator calc = new DiscountCalculator(policies);
+        
+        double discountedValue = calc.apply(100.0, CustomerType.PARTNER);
+
+        assertEquals(88.0, discountedValue, "PARTNER deveria ter 12% de desconto");
     }
-}
+    
+    }
